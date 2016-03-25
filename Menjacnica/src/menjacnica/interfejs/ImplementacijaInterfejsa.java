dbf37b682a282e0fd92288a1b0b2d0f0b69b0360
natalijaperkovic7814 <natalijaperkovic95@gmail.com>
2016-03-25 19:49:50 +0100

    Napravljena je nova klasa ImplementacijaInterfejsa, koja sadrzi postojece metode sa "praznim" telom.

---
+++ Menjacnica/src/menjacnica/interfejs/ImplementacijaInterfejsa.java
@@ -0,0 +1,23 @@
+package menjacnica.interfejs;
+
+import menjacnica.valuta.Valuta;
+
+public class ImplementacijaInterfejsa implements SpecifikacijaInterfejsa {
+
+	
+	public void dodavanjeKursa(Valuta v) {
+
+	}
+
+	
+	public void brisanjeKursa(Valuta v) {
+
+
+	}
+
+	public double nalazenjeKursa(Valuta v) {
+		
+		return 0;
+	}
+
+}
