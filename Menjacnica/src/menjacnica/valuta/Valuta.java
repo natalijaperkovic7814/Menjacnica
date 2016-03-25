5e981b3355d43fa87d35726730947190f20590c3
natalijaperkovic7814 <natalijaperkovic95@gmail.com>
2016-03-25 18:15:19 +0100

    U klasu Valuta dodate su hashCode,equals i toString metode za sve atribute.

--- Menjacnica/src/menjacnica/valuta/Valuta.java
+++ Menjacnica/src/menjacnica/valuta/Valuta.java
@@ -2,6 +2,7 @@ package menjacnica.valuta;
 
 import java.util.GregorianCalendar;
 
+
 public class Valuta {
 
 	private String naziv;
@@ -11,6 +12,7 @@ public class Valuta {
 	private double kupovni;
 	private double srednji;
 	
+
 	
 	public String getNaziv() {
 		return naziv;
@@ -47,6 +49,59 @@ public class Valuta {
 	}
 	public void setSrednji(double srednji) {
 		this.srednji = srednji;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceno == null) ? 0 : skraceno.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skraceno == null) {
+			if (other.skraceno != null)
+				return false;
+		} else if (!skraceno.equals(other.skraceno))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceno=" + skraceno + ", datum=" + datum + ", prodajni=" + prodajni
+				+ ", kupovni=" + kupovni + ", srednji=" + srednji + "]";
 	}
 	
 	
