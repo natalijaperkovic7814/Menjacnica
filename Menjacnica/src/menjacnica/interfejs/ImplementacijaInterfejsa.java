2dca4ead7702fba4c1adaf2d84a094d8448bc303
natalijaperkovic7814 <natalijaperkovic95@gmail.com>
2016-03-28 16:09:31 +0200

    Na master grani napisana drugacija metoda dodavanjeKursa.

--- Menjacnica/src/menjacnica/interfejs/ImplementacijaInterfejsa.java
+++ Menjacnica/src/menjacnica/interfejs/ImplementacijaInterfejsa.java
@@ -1,12 +1,34 @@
 package menjacnica.interfejs;
 
+import java.util.LinkedList;
+
 import menjacnica.valuta.Valuta;
 
 public class ImplementacijaInterfejsa implements SpecifikacijaInterfejsa {
 
-	
+	LinkedList<Valuta> kursevi=new LinkedList<Valuta>();
 	public void dodavanjeKursa(Valuta v) {
 
+		if(v==null){
+			throw new RuntimeException("Greska");
+		}
+			
+			Valuta v1=new Valuta();
+			v1.setNaziv(v.getNaziv());
+			v1.setSkraceno(v.getSkraceno());
+			v1.setDatum(v.getDatum());
+			v1.setKupovni(v.getKupovni());
+			v1.setProdajni(v.getProdajni());
+			v1.setSrednji(v.getSrednji());
+			boolean unet=true;
+			for(int i=0;i<kursevi.size();i++){
+				if(kursevi.get(i).equals(v1)){
+					unet=false;
+				}
+			}
+			if(unet){
+				kursevi.add(v1);
+		}
 	}
 
 	
